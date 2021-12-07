a28a386dcf078692edfa3326ad9fc50f17329780
Bartosz Rokita <ketrabx1x@gmail.com>
2021-12-07 02:10:17 +0100

    Dodaj pliki projektów.

---
+++ Postac.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PowiedzmyZeGra
+{
+    class Postac
+    {
+        string imie;
+        public int sila;
+        public int zdrowie;
+        public int charyzma;
+
+        public Postac(string imie, int sila, int zdrowie, int charyzma)
+        {
+            this.imie = imie;
+            this.sila = sila;
+            this.zdrowie = zdrowie;
+            this.charyzma = charyzma;
+        }
+        public int DodajSile()
+        {
+            return sila++;
+        }
+        public int DodajZdrowie()
+        {
+            return zdrowie++;
+        }
+        public int DodajCharyzme()
+        {
+            return charyzma++;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace PowiedzmyZeGra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Witaj!");
+            Console.WriteLine("Czy chcesz rozpocząć nową gre? T/N");
+            String czy = Console.ReadLine();
+            if (czy.Equals("N"))
+            {
+                Console.WriteLine("Koniec gry!");
+                return;
+            }
+            else if (czy.Equals("T")) ;
+            else
+            {
+                Console.WriteLine("Zła opcja!");
+                return;
+            }
+            Console.WriteLine("Witaj w kreatorze postaci"); ;
+            Console.Write("\tPodaj imie swojej postaci: ");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            String imie = Console.ReadLine();
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Postac postac = new Postac(imie, 2, 2, 2);
+            Console.Write("\tWitaj ");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write(imie);
+            Console.ResetColor();
+            Console.WriteLine();
+            int punkty = 5;
+            while (punkty > 0)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine();
+                Console.WriteLine("Twoje punkty: siła (" + postac.sila + ") zdrowie (" + postac.zdrowie + ") charyzma (" + postac.charyzma + ")");
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("Masz " + punkty + " punktów do rozdania");
+                Console.WriteLine("\t(1) +1 do siły");
+                Console.WriteLine("\t(2) +1 do zdrowia");
+                Console.WriteLine("\t(3) +1 do charyzmy");
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                String czy2 = Console.ReadLine();
+                if (czy2.Equals("1"))
+                {
+                    postac.DodajSile();
+                    punkty--;
+                }
+                else if (czy2.Equals("2"))
+                {
+                    postac.DodajZdrowie();
+                    punkty--;
+                }
+                else if (czy2.Equals("3"))
+                {
+                    postac.DodajCharyzme();
+                    punkty--;
+                }
+                else
+                {
+                    Console.WriteLine("Zła liczba");
+                }
+                Console.ResetColor();
+                if (punkty == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine("Twoje punkty: siła (" + postac.sila + ") zdrowie (" + postac.zdrowie + ") charyzma (" + postac.charyzma + ")");
+                    Console.ResetColor();
+                }
+                Console.ResetColor();
+            }
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("\nWitaj "+imie+", twoja historia zaczyna się w małej karczmie na pograniczu Zadupia " +
+                "\na Wypizgarzem. Dwiema zwaśnionymi królestwami");
+            Console.ReadKey();
+        }
+    }
+}
